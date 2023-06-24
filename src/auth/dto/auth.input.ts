c81c83f90017e64a16069a085d1e607f0d7953cf
XeniaChe <xeniacserkun@gmail.com>
2023-06-24 15:05:04 +0200

    User input validation

--- src/auth/dto/auth.input.ts
+++ src/auth/dto/auth.input.ts
@@ -1,11 +1,26 @@
 import { Field, InputType } from '@nestjs/graphql';
+import {
+  IsString,
+  IsEmail,
+  IsNotEmpty,
+  MinLength,
+  // IsStrongPassword, // Switched off while testing
+} from 'class-validator';
 
-//TODO: verify
 @InputType()
 export class AuthInput {
   @Field(() => String)
+  @IsString()
+  @IsNotEmpty()
+  @IsEmail()
   email: string;
 
   @Field(() => String)
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(13, {
+    message: 'Password shuld be min 8 characters long',
+  })
+  // @IsStrongPassword()
   password: string;
 }
